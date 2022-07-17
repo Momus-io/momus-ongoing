c9db876c33d1c266e771505d892b5ce5fe317ffa
Steve Giordano <stephenagiordano@gmail.com>
2022-07-17 12:27:37 -0400

    Updated runtime for testing

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import worker
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("cron", hour=12, minute=26)
+@sched.scheduled_job("cron", hour=12, minute=29)
 def scheduled_job():
     worker.main()
 

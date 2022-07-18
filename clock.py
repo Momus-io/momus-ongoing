c82f2aaee758a6eb1062203516972a1f80f380dc
Steve Giordano <stephenagiordano@gmail.com>
2022-07-18 09:26:40 -0400

    Still updating clock

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import time
 import worker
 
 # Schedule for Midnight GMT
-schedule.every().day.at("13:20:00").do(worker.main)
+schedule.every().day.at("13:30").do(worker.main)
 
 while True:
     schedule.run_pending()

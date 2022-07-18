0097bb1a901645556cb06ce36b8b0617a511268c
Steve Giordano <stephenagiordano@gmail.com>
2022-07-17 22:27:41 -0400

    Changed schedule to GMT

--- clock.py
+++ clock.py
@@ -2,9 +2,8 @@ import schedule
 import time
 import worker
 
-# schedule.every().day.at("22:15").do(worker.main)
-schedule.every(12).seconds.do(worker.main)
-
+# Schedule for Midnight GMT
+schedule.every().day.at("04:00:00").do(worker.main)
 
 while True:
     schedule.run_pending()

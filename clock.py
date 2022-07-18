22b6082630ad75456da519dd4ec5d8e347d69247
Steve Giordano <stephenagiordano@gmail.com>
2022-07-17 21:56:24 -0400

    Changed clock logic

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ import schedule
 import time
 import worker
 
-schedule.every().day.at("21:55").do(worker.main)
+schedule.every().day.at("21:58").do(worker.main)
 
 while True:
     schedule.run_pending()

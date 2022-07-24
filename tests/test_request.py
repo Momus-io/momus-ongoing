16dfe40f2ad02c567a40638a1423aaeb4fac8bfa
Steve Giordano <stephenagiordano@gmail.com>
2022-07-24 19:20:11 -0400

    Updated test file

--- tests/test_request.py
+++ tests/test_request.py
@@ -4,11 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-TEST_DATE = "2011-08-01"
-
 
 def test_request():
-    global TEST_DATE
+    TEST_DATE = "2011-08-01"
 
     base_url = f"https://api.twitter.com/2/users/133110529/tweets?max_results=100&start_time={TEST_DATE}T00:00:00Z&end_time={TEST_DATE}T23:59:59Z&tweet.fields=created_at&exclude=retweets"
 
